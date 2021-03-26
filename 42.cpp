1527488ecf1b0effba6a1ce62a1e41c82975dd2c
Siannodel <826309057@qq.com>
2021-03-26 22:26:07 +0800

    .

---
+++ 42.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> left_max(n, 0);
+        vector<int> right_max(n, 0);
+        int l = 0;
+        for(int i = 1; i < n; i++)
+        {
+            if (height[i-1] > l)
+            {
+                l = height[i-1];
+            }
+            left_max[i] = l;
+        }
+        int r = 0;
+        for (int i = n-2; i >= 0; i--)
+        {
+            if (height[i+1]>r)
+            {
+                r = height[i+1];
+            }
+            right_max[i] = r;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int tmp = min(right_max[i], left_max[i])- height[i];
+            ans += tmp > 0 ? tmp : 0;
+        }
+        return ans;
+    }
+};
